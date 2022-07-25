658a5ce8fbf19e2606e3dab35b9864fa0743f1bb
Ayberk Yiğit <kurtknispel9@gmail.com>
2022-07-25 13:51:19 +0300

    fixed a problem with navigation widget being rendered on home widget added photos for carousel

--- lib/Pages/Home.dart
+++ lib/Pages/Home.dart
@@ -4,10 +4,21 @@ import 'package:blog/utils/Navigation.dart';
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Container(
+  //     child: Navigation(),
+  //   );
+  // }
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Navigation(),
+    return Column(
+      children: [
+        SizedBox(
+          height: 930.0,
+          child: Navigation(),
+        )
+      ],
     );
   }
 }
