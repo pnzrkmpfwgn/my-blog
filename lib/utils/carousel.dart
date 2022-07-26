4583ea17d3e47221f4b28001d4f452e369fcb1bf
Ayberk Yiğit <kurtknispel9@gmail.com>
2022-07-26 16:47:57 +0300

    slider to fallback

--- lib/utils/carousel.dart
+++ lib/utils/carousel.dart
@@ -2,26 +2,113 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
 class Carousel extends StatelessWidget {
-  const Carousel({Key? key}) : super(key: key);
+  List<String> imgList=[
+    "assets/1.jpg",
+    "assets/2.jpg",
+    "assets/3.jpg",
+
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return CarouselSlider(
-      options: CarouselOptions(height: 400.0),
-      items: [1,2,3,4,5].map((i) {
-        return Builder(
-          builder: (BuildContext context) {
-            return Container(
-                width: MediaQuery.of(context).size.width,
-                margin: EdgeInsets.symmetric(horizontal: 5.0),
-                decoration: BoxDecoration(
-                    color: Colors.amber
-                ),
-                child: Text('text $i', style: TextStyle(fontSize: 16.0),)
-            );
-          },
-        );
-      }).toList(),
+  return Container(
+  child: CarouselSlider(
+  options: CarouselOptions(),
+  items: imgList
+      .map((item) => Container(
+  child: Stack(children:[
+    Image.asset(
+        item,
+        width: MediaQuery.of(context).size.width,
+        height:MediaQuery.of(context).size.height,
+        fit: BoxFit.fill,
+    ),
+    Positioned(
+        child:Text(
+            "Lorem ipsum dolor sit amet",
+        style:TextStyle(
+            fontSize:60.0,
+          color:Colors.white,
+          fontFamily: "DM Serif Text"
+        )
+        ),
+      top: 100,
+      left:20
+
+    )
+  ]),
+  ))
+      .toList(),
+  ));
+  }
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     body: Builder(
+  //       builder: (context) {
+  //         final double height = MediaQuery.of(context).size.height;
+  //         return CarouselSlider(
+  //           options: CarouselOptions(
+  //             height: height,
+  //             viewportFraction: 1.0,
+  //             enlargeCenterPage: false,
+  //             // autoPlay: false,
+  //           ),
+  //           items: imgList
+  //               .map((item) => Container(
+  //             child: Center(
+  //                 child: Image.asset(
+  //                   item,
+  //                   fit: BoxFit.fill,
+  //                   height: height,
+  //                 )
+  //             ),
+  //           ))
+  //               .toList(),
+  //         );
+  //       },
+  //     ),
+  //   );
+  // }
+}
+
+class mobileCarousel extends StatelessWidget {
+  mobileCarousel({Key? key}) : super(key: key);
+
+  final List<String> imgList=[
+    "assets/1.jpg",
+    "assets/2.jpg",
+    "assets/3.jpg",
+
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Builder(
+        builder: (context) {
+          final double height = MediaQuery.of(context).size.height;
+          return CarouselSlider(
+            options: CarouselOptions(
+              height: height,
+              viewportFraction: 1.0,
+              enlargeCenterPage: false,
+              // autoPlay: false,
+            ),
+            items: imgList
+                .map((item) => Container(
+              child: Center(
+                  child: Image.asset(
+                    item,
+                    fit: BoxFit.cover,
+                    height: height,
+                  )),
+            ))
+                .toList(),
+          );
+        },
+      ),
     );
   }
 }
+
