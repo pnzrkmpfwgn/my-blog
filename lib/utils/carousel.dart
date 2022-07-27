a3e6c48985e4f1fc15864c148c6c4155f743acb7
Ayberk Yiğit <kurtknispel9@gmail.com>
2022-07-27 15:25:50 +0300

    Adjusted the titles of the slider got rid of unnecessary comments

--- lib/utils/carousel.dart
+++ lib/utils/carousel.dart
@@ -1,93 +1,101 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
-class Carousel extends StatelessWidget {
+class Carousel extends StatefulWidget {
+  const Carousel({Key? key}) : super(key: key);
+
+  @override
+  State<Carousel> createState() => _CarouselState();
+}
+
+class _CarouselState extends State<Carousel> {
   List<String> imgList=[
     "assets/1.jpg",
     "assets/2.jpg",
     "assets/3.jpg",
-
   ];
 
   @override
   Widget build(BuildContext context) {
-  return Container(
-  child: CarouselSlider(
-  options: CarouselOptions(),
-  items: imgList
-      .map((item) => Container(
-  child: Stack(children:[
-    Container(
-      decoration:
-      BoxDecoration(
-        image: DecorationImage(
-          fit:BoxFit.cover,
-          colorFilter:ColorFilter.mode(Colors.black.withOpacity(1),BlendMode.dstATop),
-          image: AssetImage('${item}'),
-        ),
-      ),
-    ),
-     //   Image.asset(
-     //     item,
-     //     width: MediaQuery.of(context).size.width,
-     //     height:MediaQuery.of(context).size.height,
-     //     fit: BoxFit.fill,
-     //     color: Colors.black.withOpacity(0.01),
-     // ),
-    Positioned(
-        child:Container(
-            child:Text(
-            "Lorem ipsum dolor sit amet",
-        style:TextStyle(
-            fontSize:60.0,
-          color:Colors.white,
-          fontFamily: "DM Serif Text"
-        )
-        ),
-    ),
-        top: 100,
-        left:20
-    )
-  ]),
-  ))
-      .toList(),
-  ));
+    return Container(
+        child: CarouselSlider(
+          options: CarouselOptions(),
+          items: imgList
+              .map((item) => Container(
+            child: Stack(children:[
+              Container(
+                decoration:
+                BoxDecoration(
+                  image: DecorationImage(
+                    fit:BoxFit.cover,
+                    colorFilter:ColorFilter.mode(Colors.black.withOpacity(1),BlendMode.dstATop),
+                    image: AssetImage('${item}'),
+                  ),
+                ),
+              ),
+              //   Image.asset(
+              //     item,
+              //     width: MediaQuery.of(context).size.width,
+              //     height:MediaQuery.of(context).size.height,
+              //     fit: BoxFit.fill,
+              //     color: Colors.black.withOpacity(0.01),
+              // ),
+              Positioned(
+                  child:Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Text("Category    ",
+                                    style:TextStyle(
+                                        color:Colors.white.withOpacity(0.8)
+                                    )
+                                ),
+                                Text("Posted by  ",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color:Colors.white
+                                  ),
+                                ),
+                                Text("Name",
+                                  style: TextStyle(
+                                      color:Colors.white.withOpacity(0.8)
+                                  ),
+                                )
+                              ],
+                            ),
+                            Text(
+                                "Lorem ipsum dolor sit amet",
+                                style:TextStyle(
+                                    fontSize:60.0,
+                                    color:Colors.white,
+                                    fontFamily: "DMSerifText"
+                                )
+                            ),
+                          ],),
+
+                  ),
+                  top: 350,
+                  left:250
+              )
+            ]),
+          ))
+              .toList(),
+        ));
   }
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     body: Builder(
-  //       builder: (context) {
-  //         final double height = MediaQuery.of(context).size.height;
-  //         return CarouselSlider(
-  //           options: CarouselOptions(
-  //             height: height,
-  //             viewportFraction: 1.0,
-  //             enlargeCenterPage: false,
-  //             // autoPlay: false,
-  //           ),
-  //           items: imgList
-  //               .map((item) => Container(
-  //             child: Center(
-  //                 child: Image.asset(
-  //                   item,
-  //                   fit: BoxFit.fill,
-  //                   height: height,
-  //                 )
-  //             ),
-  //           ))
-  //               .toList(),
-  //         );
-  //       },
-  //     ),
-  //   );
-  // }
 }
 
-class mobileCarousel extends StatelessWidget {
-  mobileCarousel({Key? key}) : super(key: key);
+class mobileCarousel extends StatefulWidget {
+  const mobileCarousel({Key? key}) : super(key: key);
 
-  final List<String> imgList=[
+  @override
+  State<mobileCarousel> createState() => _mobileCarouselState();
+}
+
+class _mobileCarouselState extends State<mobileCarousel> {
+
+  List<String> imgList=[
     "assets/1.jpg",
     "assets/2.jpg",
     "assets/3.jpg",
@@ -116,24 +124,43 @@ class mobileCarousel extends StatelessWidget {
                   ),
                 ),
               ),
-              //   Image.asset(
-              //     item,
-              //     width: MediaQuery.of(context).size.width,
-              //     height:MediaQuery.of(context).size.height,
-              //     fit: BoxFit.fill,
-              //     color: Colors.black.withOpacity(0.01),
-              // ),
+
               Positioned(
-                  child:Text(
-                      "Lorem ipsum dolor sit amet",
-                      style:TextStyle(
-                          fontSize:60.0,
-                          color:Colors.white,
-                          fontFamily: "DM Serif Text"
-                      )
+                  child:Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text("Category    ",
+                              style:TextStyle(
+                                  color:Colors.white.withOpacity(0.8)
+                              )
+                          ),
+                          Text("Posted by  ",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color:Colors.white
+                            ),
+                          ),
+                          Text("Name",
+                            style: TextStyle(
+                                color:Colors.white.withOpacity(0.8)
+                            ),
+                          )
+                        ],
+                      ),
+                      Text(
+                          "Lorem ipsum dolor sit amet",
+                          style:TextStyle(
+                              fontSize:60.0,
+                              color:Colors.white,
+                              fontFamily: "DMSerifText"
+                          )
+                      ),
+                    ],
                   ),
-                  top: 100,
-                  left:20
+                  top: 350,
+                  left:250
 
               )
             ]),
