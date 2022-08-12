d7495eafe4e3deff5a0692b801466bd20ef31d05
Ayberk Yiğit <kurtknispel9@gmail.com>
2022-08-12 22:46:21 +0300

    Finished the post instance and contemplated suicide

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:blog/Pages/Categories.dart';
 import 'package:blog/Pages/About.dart';
 import 'package:blog/Pages/Contact.dart';
 import 'package:flutter/material.dart';
+import 'package:blog/posts/Post1.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -11,8 +12,9 @@ void main() {
       '/home':(context) => Home(),
       '/categories':(context) => Categories(),
       '/about':(context) => About(),
-      '/contact':(context) => Contact()
-    },
+      '/contact':(context) => Contact(),
+      '/post1':(context) => Post1()
+     },
   ));
 }
 
